84958b7775b7c2cd13d8d35635212450a0630dab
Edson da Costa Cordeiro <edsoncordeiro@MacBook-Air-de-Edson.local>
2026-01-14 18:36:25 -0300

    la se vai a nova tentativa

---
+++ LearnGit/main.swift
@@ -0,0 +1,14 @@
+//
+//  main.swift
+//  LearnGit
+//
+//  Created by Edson da Costa Cordeiro on 14/01/26.
+//
+
+import Foundation
+
+print("Hello, World!")
+print("A terra é azul!")
+print("O lago está vazio")
+
+
